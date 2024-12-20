e2970197adbc9ee812ac3015e194e5e3a76e47b4
Ahmed <norbixin@live.com>
2024-12-20 17:08:08 +0300

    :tada: initial commit

---
+++ app.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "@tanstack/start/config";
+import { cloudflare } from "unenv";
+
+export default defineConfig({
+  server: {
+    preset: "cloudflare-pages",
+    unenv: cloudflare,
+    rollupConfig: {
+      external: ["node:async_hooks"],
+    },
+  },
+});
