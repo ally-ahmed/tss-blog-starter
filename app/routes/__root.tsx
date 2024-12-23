1994037ed9adcc147335d4debc66e9092bd52ef7
Ahmed <norbixin@live.com>
2024-12-23 11:16:04 +0300

    remove local font

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -10,7 +10,7 @@ import { DefaultCatchBoundary } from "@/components/DefaultCatchBoundary";
 import { NotFound } from "@/components/NotFound";
 import globalStyle from "@/styles/globals.css?url";
 import fontsourceInter from "@fontsource-variable/inter?url";
-// import calSans from "cal-sans?url";
+import calSans from "cal-sans?url";
 
 export const Route = createRootRoute({
   head: () => ({
@@ -36,9 +36,8 @@ export const Route = createRootRoute({
         href: fontsourceInter,
       },
       {
-        rel: "preload",
-        href: "/fonts/CalSans-SemiBold.ttf",
-        as: "font",
+        rel: "stylesheet",
+        href: calSans,
       },
     ],
   }),
