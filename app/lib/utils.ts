98876f737b63b975a29eb511d90dd9f8cf1d1ab5
Ahmed <norbixin@live.com>
2025-01-05 20:46:04 +0300

    remove unused code and deps

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -24,26 +24,3 @@ export const sortedPosts = allPosts.sort(
 );
 
 export type Posts = typeof allPosts;
-
-export function createContextFactory<ContextData>(options?: {
-  defaultValue?: ContextData | null;
-  errorMessage?: string;
-}) {
-  const opts = {
-    defaultValue: null,
-    errorMessage: "useContext must be used within a Provider",
-    ...options,
-  };
-
-  const context = createContext<ContextData | null>(opts.defaultValue);
-
-  function useContextFactory(): ContextData {
-    const contextValue = useContext(context);
-    if (contextValue === null) {
-      throw new Error(opts.errorMessage);
-    }
-    return contextValue;
-  }
-
-  return [context.Provider, useContextFactory] as const;
-}
