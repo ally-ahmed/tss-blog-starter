7fd3e288bfdcf7f1624ab761ddf9c53cb5a5051a
Ahmed <norbixin@live.com>
2025-01-05 20:49:25 +0300

    made default theme to light

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -99,7 +99,7 @@ function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
         <Meta />
       </head>
       <body className="min-h-screen bg-background font-sans antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <ThemeProvider defaultTheme="system">
+        <ThemeProvider defaultTheme="light">
           <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0 min-h-screen">
             <Header />
             {children}
